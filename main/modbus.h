37de8c328da2fb8361c8b1398681316d664c87d3
Rikka0w0 <929513338@qq.com>
2021-01-09 07:08:45 +1100

    Add compatibility mode due to a common TCP client bug

--- main/modbus.h
+++ main/modbus.h
@@ -4,6 +4,16 @@
 #include <stdint.h>
 #include <strings.h>
 
+// If the compatibility mode is enabled, the response will be send
+// to the TCP client via a single send(). Some MODBUS TCP client
+// assumes recv() and send() are paired, this is a common mistake,
+// TCP streams bytes, recv() and send() does not define packets.
+// A correct TCP client implementation should allow a frame to be
+// sent via multiple send().
+// NOTE: Enable this option will increase the RAM usage (stack),
+// the unnecessary memcpy() may compromise the performance.
+#define MODBUS_BRIDGE_COMPATIBILITY_MODE
+
 #define MODBUS_RTU_PDU_MAXLEN       252
 #define MODBUS_RTU_FRAME_MAXLEN     256
 #define MODBUS_RTU_TX_FIFO_LEN      (MODBUS_RTU_FRAME_MAXLEN*2)
@@ -31,6 +41,6 @@ void modbus_uart_init();
 // Attempt to queue a new request to the fifo, non-blocking
 int modbus_uart_fifo_push(const rtu_session_t* session_header, const void* payload, size_t len);
 // Queue the response to the Tx FIFO of TCP, non-blocking
-void tcp_server_send_response(const rtu_session_t* session_header, const void* buf, size_t len);
+void tcp_server_send_response(const rtu_session_t* session_header, const void* payload, size_t len);
 
 #endif /* MAIN_MODBUS_H_ */
