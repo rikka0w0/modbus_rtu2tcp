a8dbe9c2c72c25d1217296e524bef19d1b209e52
Rikka0w0 <929513338@qq.com>
2021-01-20 12:11:49 +1100

    Fix a buffer overflow problem

--- main/modbus_rtu2tcp_main.c
+++ main/modbus_rtu2tcp_main.c
@@ -400,9 +400,8 @@ esp_err_t wifi_query_ip_info(uint8_t sta_0_ap_1, ip_info_t* ip_info) {
         ip_info->ip6_count = 0;
         for (size_t i=0; i<LWIP_IPV6_NUM_ADDRESSES; i++) {
             if (ip6_addr_isvalid(netif->ip6_addr_state[i])) {
-                snprintf(ip_info->ip6_addr[i], IPV6_ADDR_MAXLEN, IPV6STR, IPV62STR(netif->ip6_addr[i].u_addr.ip6));
-                ip_info->ip6_addr[i][IPV6_ADDR_MAXLEN-1] = '\0';
-                ip_info->ip6_count++;
+                snprintf(ip_info->ip6_addr[ip_info->ip6_count++], IPV6_ADDR_MAXLEN,
+                         IPV6STR, IPV62STR(netif->ip6_addr[i].u_addr.ip6));
             }
         }
 
